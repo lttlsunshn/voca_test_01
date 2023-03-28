af2f539634a99c36e685f686351738c143943720
lttlsunshn <121416291+lttlsunshn@users.noreply.github.com>
2023-03-29 01:52:02 +0900

    Update firebase.js

--- src/api/firebase.js
+++ src/api/firebase.js
@@ -5,7 +5,9 @@ import { get, getDatabase, ref, set } from "firebase/database";
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  // databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  databaseURL:
+    "https://voca-test-01-default-rtdb.asia-southeast1.firebasedatabase.app",
   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
 };
 
