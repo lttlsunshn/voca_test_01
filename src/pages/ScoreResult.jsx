64f9f9ca5aa57cab498395ec8bf00102d3a9f0ef
lttlsunshn <121416291+lttlsunshn@users.noreply.github.com>
2023-04-06 23:30:18 +0900

    scoreResult update

--- src/pages/ScoreResult.jsx
+++ src/pages/ScoreResult.jsx
@@ -1,13 +1,16 @@
 import { useQuery } from "@tanstack/react-query";
 import React, { useContext } from "react";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import { getAnswerList } from "../api/firebase";
-import { SortStateContext } from "../SortContext";
+import { SortDispatchContext, SortStateContext } from "../SortContext";
+import { FaArrowLeft } from "react-icons/fa";
 
 let correctNum = 0;
 
 export default function ScoreResult() {
   const sortState = useContext(SortStateContext);
+  const dispatch = useContext(SortDispatchContext);
+  const navigate = useNavigate();
 
   const { noteTitle } = useParams();
   const { timeTitle } = useParams();
@@ -25,10 +28,51 @@ export default function ScoreResult() {
   correctNum =
     scoreResult && scoreResult.filter((item) => item.isCorrect).length;
 
+  const handleOnlineBtn = () => {
+    dispatch({ type: "test" });
+    navigate(`/voca-notes/${noteTitle}/online-test`);
+  };
+
   return (
     <>
       <div className="voca_note_header">
         <div className="voca_note_title">{noteTitle} Online TEST Result</div>
+        <div className="button-list">
+          <button onClick={handleOnlineBtn}>
+            <FaArrowLeft />
+          </button>
+        </div>
+      </div>
+      <div className="list-options">
+        <div className="toggle" id="toggle-spell-disabled">
+          <input
+            id="btn_toggle_spell"
+            type="radio"
+            name="test-toggle"
+            value="spell"
+          />
+          <label htmlFor="btn_toggle_spell" value="spell">
+            철자
+          </label>
+        </div>
+        <div className="toggle" id="toggle-meaning-disabled">
+          <input id="btn_toggle_meaning" type="radio" name="test-toggle" />
+          <label htmlFor="btn_toggle_meaning">뜻</label>
+        </div>
+        <div className="sort-btn-list">
+          <div className="sort_disabled">
+            <input id="btn_sort_asc" type="radio" name="sort_btn" />
+            <label htmlFor="btn_sort_asc">오름차순</label>
+          </div>
+          <div className="sort_disabled">
+            <input id="btn_sort_desc" type="radio" name="sort_btn" />
+            <label htmlFor="btn_sort_desc">내림차순</label>
+          </div>
+          <div className="sort_disabled">
+            <input id="btn_sort_random" type="radio" name="sort_btn" />
+            <label htmlFor="btn_sort_random">랜덤</label>
+          </div>
+        </div>
       </div>
       <form>
         <table className="voca_note">
