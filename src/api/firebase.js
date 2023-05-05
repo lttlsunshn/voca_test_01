ec9c7563651a6a720fd85be7eff5abc45aa8a083
lttlsunshn <121416291+lttlsunshn@users.noreply.github.com>
2023-05-05 23:40:48 +0900

    UPDATE: firebase.js error handling

--- src/api/firebase.js
+++ src/api/firebase.js
@@ -15,59 +15,100 @@ const app = initializeApp(firebaseConfig);
 const db = getDatabase(app);
 
 export async function addNewNote(inputTitle, createdTime, createdTimeNum) {
-  return set(ref(db, `voca-notes/${createdTimeNum}`), {
-    noteTitle: inputTitle,
-    createdTime,
-    id: createdTimeNum,
-  });
+  try {
+    await set(ref(db, `voca-notes/${createdTimeNum}`), {
+      noteTitle: inputTitle,
+      createdTime,
+      id: createdTimeNum,
+    });
+  } catch (error) {
+    console.error("Error occurred while adding a new note: ", error);
+  }
 }
 
 export async function getNote(noteId) {
-  return get(ref(db, `voca-notes/${noteId}`)).then((snapshot) => {
-    if (snapshot.exists()) {
-      return snapshot.val();
-    }
-    return [];
-  });
+  let snapshot;
+
+  try {
+    snapshot = await get(ref(db, `voca-notes/${noteId}`));
+  } catch (error) {
+    snapshot = [];
+    console.error("Error occurred while getting a note.", error);
+  }
+  return snapshot.val();
 }
 
 export async function updateNote(noteModify, noteIdModify, inputTitle) {
-  return update(ref(db, `voca-notes/${noteIdModify}/`), {
-    ...noteModify,
-    noteTitle: inputTitle,
-  });
+  try {
+    await update(ref(db, `voca-notes/${noteIdModify}/`), {
+      ...noteModify,
+      noteTitle: inputTitle,
+    });
+  } catch (error) {
+    return console.error(
+      `Error occcured while updating note ${inputTitle}.`,
+      error
+    );
+  }
 }
 export async function deleteNote(noteTitle) {
-  return remove(ref(db, `voca-notes/${noteTitle}`));
+  try {
+    await remove(ref(db, `voca-notes/${noteTitle}`));
+  } catch (error) {
+    return console.error(
+      `Error occured while deleting note ${noteTitle}.`,
+      error
+    );
+  }
 }
 
 export async function addNewWord(noteId, word_eng, num, word_kor, id) {
-  return set(ref(db, `voca-notes/${noteId}/wordList/` + word_eng), {
-    num,
-    word_eng,
-    word_kor,
-    id,
-  });
+  try {
+    await set(ref(db, `voca-notes/${noteId}/wordList/` + word_eng), {
+      num,
+      word_eng,
+      word_kor,
+      id,
+    });
+  } catch (error) {
+    return console.error(
+      `Error occured while adding a new word ${word_eng}. `,
+      error
+    );
+  }
 }
 
 export async function modifyWord(noteId, word_eng, num, word_kor, id) {
-  return update(ref(db, `voca-notes/${noteId}/wordList/` + word_eng), {
-    num,
-    word_eng,
-    word_kor,
-    id,
-  });
+  try {
+    await update(ref(db, `voca-notes/${noteId}/wordList/` + word_eng), {
+      num,
+      word_eng,
+      word_kor,
+      id,
+    });
+  } catch (error) {
+    return console.error(`Error occured while modifying a word. `, error);
+  }
 }
 
 export async function deleteWord(noteId, word_eng) {
-  return remove(ref(db, `voca-notes/${noteId}/wordList/` + word_eng));
+  try {
+    await remove(ref(db, `voca-notes/${noteId}/wordList/` + word_eng));
+  } catch (error) {
+    return console.error(`Error occured while deleting a word. `, error);
+  }
 }
 
 export async function makeAnswerTitle(noteId, createdTime) {
-  return set(ref(db, `online-test/test-${noteId}-${createdTime}`), {
-    createdTime,
-    noteId,
-  });
+  try {
+    await set(ref(db, `online-test/test-${noteId}-${createdTime}`), {
+      createdTime,
+      noteId,
+    });
+    console.log("An answer Title was made successfully. ");
+  } catch (error) {
+    console.error(`Error occured while making an answer title.`, error);
+  }
 }
 
 export async function makeAnswerList(
@@ -78,38 +119,51 @@ export async function makeAnswerList(
   item,
   idx
 ) {
-  return set(
-    ref(
-      db,
-      `online-test/test-${noteId}-${createdTime}/answer-list/` + item.num
-    ),
-    {
-      answer,
-      isCorrect,
-      num: item.num,
-      word_eng: item.word_eng,
-      word_kor: item.word_kor,
-      order: idx,
-    }
-  );
+  try {
+    await set(
+      ref(
+        db,
+        `online-test/test-${noteId}-${createdTime}/answer-list/` + item.num
+      ),
+      {
+        answer,
+        isCorrect,
+        num: item.num,
+        word_eng: item.word_eng,
+        word_kor: item.word_kor,
+        order: idx,
+      }
+    );
+    console.log("An answer List was made successfully.");
+  } catch (error) {
+    console.error("Error occurred while making answer list:", error);
+  }
 }
 
 export async function getAnswerList(noteId, timeTitle) {
-  return get(
-    ref(db, `online-test/test-${noteId}-${timeTitle}/answer-list`)
-  ).then((snapshot) => {
-    if (snapshot.exists()) {
-      return Object.values(snapshot.val());
-    }
-    return [];
-  });
+  let snapshot;
+
+  try {
+    snapshot = await get(
+      ref(db, `online-test/test-${noteId}-${timeTitle}/answer-list`)
+    );
+  } catch (error) {
+    snapshot = [];
+    console.error("Error occurred while getting notes", error);
+  }
+
+  return Object.values(snapshot.val());
 }
 
 export async function getNotes() {
-  return get(ref(db, "voca-notes")).then((snapshot) => {
-    if (snapshot.exists()) {
-      return snapshot.val();
-    }
-    return [];
-  });
+  let snapshot;
+
+  try {
+    snapshot = await get(ref(db, "voca-notes"));
+  } catch (error) {
+    snapshot = [];
+    console.error("Error occurred while getting notes", error);
+  }
+
+  return snapshot.val();
 }
